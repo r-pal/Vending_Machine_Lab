91d3a029cbedebdd6e839b4a6ec2a9b367376035
dazzle-tactics <90906037+dazzle-tactics@users.noreply.github.com>
2022-03-23 15:06:52 +0000

    dont know

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+
 public class VendingMachine {
 
+    private ArrayList<Drawer> drawers;
+    private ArrayList<Coin> coinsEntered;
+    private CoinReturn coinReturn;
 
+    public VendingMachine(ArrayList<Drawer> drawers, ArrayList<Coin> coinsEntered, CoinReturn coinReturn) {
+        this.drawers = drawers;
+        this.coinsEntered = coinsEntered;
+        this.coinReturn = coinReturn;
+    }
+
+    public ArrayList<Drawer> getDrawers() {
+        return drawers;
+    }
+
+    public void setDrawers(ArrayList<Drawer> drawers) {
+        this.drawers = drawers;
+    }
+
+    public ArrayList<Coin> getCoinsEntered() {
+        return coinsEntered;
+    }
+
+    public void setCoinsEntered(ArrayList<Coin> coinsEntered) {
+        this.coinsEntered = coinsEntered;
+    }
+
+    public CoinReturn getCoinReturn() {
+        return coinReturn;
+    }
+
+    public void setCoinReturn(CoinReturn coinReturn) {
+        this.coinReturn = coinReturn;
+    }
+
+    public void addCoin(Coin coin){
+        this.coinsEntered.add(coin);
+    }
 }
